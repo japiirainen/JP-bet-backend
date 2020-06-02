90ca64584cf21d19a5bfabc0a9fa57b27c5ba005
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-02 16:03:10 +0300

    API in progress

--- backend/src/app.js
+++
@@ -1,3 +0,0 @@
-const express = require('express')
-const morgan = require('morgan')
-const compression = require('compression')
---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(morgan, ('tiny'))
+app.use(compression())
+app.use(helmet())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        message: '💰Welcome to JP-BET API!💰'
+    })
+})
+
+module.exports = app
