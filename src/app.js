a13300757cddf7336901df852e229a2d55da447d
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-03 21:57:04 +0300

    feat: file structure setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import morgan from 'morgan'
 import compression from 'compression'
+import { json, urlencoded } from 'body-parser'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use(compression())
 app.use(helmet())
-app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({
