ad54df99a505f2d0e660670ad9b10385e7df38b4
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-04 10:54:17 +0300

    feat: match schema

---
+++ src/recources/match/match.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const matchSchema = new mongoose.Schema(
+    {
+        id: {
+            type: mongoose.SchemaTypes.ObjectId,
+            required: true,
+            trim: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        tournament: {
+            type: String,
+            required: true,
+        },
+        team1: {
+            type: String,
+            required: true,
+        },
+        team2: {
+            type: String,
+            required: true,
+        },
+        odds: {
+            team1Win: {
+                type: Number,
+                require: true,
+            },
+            team2Win: {
+                type: Number,
+                required: true,
+            },
+        },
+    },
+
+    { timestamps: true }
+)
+
+export const Match = mongoose.model('match', matchSchema)
