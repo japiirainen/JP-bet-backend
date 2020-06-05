823cd102ce9ff19f7bc8c0a04b6e2b4693fff186
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-05 17:03:55 +0300

    feat: CRUD for match done

--- src/recources/match/match.router.js
+++ src/recources/match/match.router.js
@@ -3,6 +3,8 @@ import controllers from './match.controllers'
 
 const router = Router()
 
-router.route('/').post(controllers.createOne)
+router.route('/').post(controllers.createOne).get(controllers.getMany)
+
+router.route('/:id').get(controllers.getOne).put(controllers.updateOne).delete(controllers.removeOne)
 
 export default router
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,28 @@
-/* export const getOne = model => async (req, res) {
-    try {
-        const doc = await model.findOne({  })
-    } catch (e) {
-        
-    }
-} */
+export const getOne = model => async (req, res) => {
+	try {
+		const doc = await model.findOne({ _id: req.params.id }).lean().exec()
 
-//export const gotMany
+		if (!doc) return res.status(400).end()
+
+		res.status(200).json({ data: doc })
+	} catch (e) {
+		console.error(e)
+		res.status(400).end()
+	}
+}
+
+export const getMany = model => async (req, res) => {
+	try {
+		const docs = await model.find().lean().exec()
+
+		if (!docs) return res.status(400).end()
+
+		res.status(200).json({ data: docs })
+	} catch (e) {
+		console.error(e)
+		res.status(400).end()
+	}
+}
 
 export const createOne = model => async (req, res) => {
 	try {
@@ -18,6 +34,36 @@ export const createOne = model => async (req, res) => {
 	}
 }
 
+export const updateOne = model => async (req, res) => {
+	try {
+		const updatedDoc = await model.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).lean().exec()
+
+		if (!updatedDoc) return res.status(400).end()
+
+		res.status(200).json({ data: updatedDoc })
+	} catch (e) {
+		console.error(e)
+		res.status(400).end()
+	}
+}
+
+export const removeOne = model => async (req, res) => {
+	try {
+		const removed = await model.findOneAndRemove({ _id: req.params.id }).lean().exec()
+
+		if (!removed) return res.status(400).end()
+
+		res.status(200).json({ data: removed })
+	} catch (e) {
+		console.error(e)
+		res.status(400).end()
+	}
+}
+
 export const crudControllers = model => ({
-	createOne: createOne(model)
+	createOne: createOne(model),
+	getOne: getOne(model),
+	getMany: getMany(model),
+	updateOne: updateOne(model),
+	removeOne: removeOne(model)
 })
