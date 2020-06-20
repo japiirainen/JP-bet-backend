50db0120bb2154d93155549da2dfe82b959f7088
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-20 14:20:25 +0300

    feat: yup validation for signup

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,75 +1,99 @@
 import { User } from '../recources/user/user.model'
 import jwt from 'jsonwebtoken'
 import config from './config'
+import * as Yup from 'Yup'
+
+const schema = Yup.object().shape({
+    email: Yup.string().trim().email().required(),
+    password: Yup.string()
+        .min(8)
+        .max(100)
+        .matches(/[^A-Za-z-0-9]/, 'password must contain a special character')
+        .matches(/[A-Z]/, 'password must contain an uppercase letter')
+        .matches(/[a-z]/, 'password must contain a lowercase letter')
+        .matches(/[0-9]/, 'password must contain a number')
+        .required(),
+    username: Yup.string().trim().min(2).required(),
+})
 
 export const newToken = user => {
-	return jwt.sign(
-		{
-			id: user.id,
-			username: user.username,
-			email: user.email
-		},
-		config.secrets.jwt,
-		{
-			expiresIn: config.secrets.jwtExp
-		}
-	)
+    return jwt.sign(
+        {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+        },
+        config.secrets.jwt,
+        {
+            expiresIn: config.secrets.jwtExp,
+        }
+    )
 }
 
 export const verifyToken = token =>
-	new Promise((resolve, reject) => {
-		jwt.verify(token, config.secrets.jwt, (err, payload) => {
-			if (err) return reject(err)
-			resolve(payload)
-		})
-	})
+    new Promise((resolve, reject) => {
+        jwt.verify(token, config.secrets.jwt, (err, payload) => {
+            if (err) return reject(err)
+            resolve(payload)
+        })
+    })
 
-export const signup = async (req, res) => {
-	if (!req.body.email || !req.body.password) {
-		res.status(400).send({ message: 'Invalid email or password!' })
-	}
-	try {
-		const user = await User.create(req.body)
-		const token = newToken(user)
-		return res.status(201).send({ token })
-	} catch (e) {
-		console.error(e)
-		return res.status(400).end()
-	}
+export const signup = async (req, res, next) => {
+    const { email, password, username } = req.body
+    const createUser = { email, password, username }
+    try {
+        await schema.validate(createUser, {
+            abortEarly: false,
+        })
+        const insertedUser = await User.create({
+            email,
+            password,
+            username,
+        })
+        const token = newToken(insertedUser)
+        return res.status(201).send({ token })
+    } catch (e) {
+        res.status(400)
+        next(e)
+    }
 }
 
 export const signin = async (req, res) => {
-	if (!req.body.email || !req.body.password) {
-		res.status(400).send({ message: 'Email and password required.' })
-	}
-	const user = await User.findOne({ email: req.body.email })
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({ message: 'Email and password required.' })
+    }
+    const user = await User.findOne({ email: req.body.email })
 
-	if (!user) return res.status(401).end()
+    if (!user) return res.status(401).end()
 
-	try {
-		const pswMatch = await user.checkPassword(req.body.password)
-		if (!pswMatch) return res.status(401).send({ message: 'Invalid password!' })
+    try {
+        const pswMatch = await user.checkPassword(req.body.password)
+        if (!pswMatch)
+            return res.status(401).send({ message: 'Invalid password!' })
 
-		const token = newToken(user)
-		return res.status(201).send({ token })
-	} catch (e) {
-		console.error(e)
-		return res.status(400).end()
-	}
+        const token = newToken(user)
+        return res.status(201).send({ token })
+    } catch (e) {
+        console.error(e)
+        return res.status(400).end()
+    }
 }
 
 export const verify = async (req, res, next) => {
-	if (!req.headers.authorization) return res.status(401).end()
-	const token = req.headers.authorization.split('Bearer ')[1]
-	if (!token) return res.status(401).end()
-	try {
-		const payload = await verifyToken(token)
-		console.log('payload', payload)
-		const user = await User.findById(payload.id).select('-password').lean().exec()
-		req.user = user
-		next()
-	} catch (e) {
-		console.error(e)
-		return res.status(401).end()
-	}
+    if (!req.headers.authorization) return res.status(401).end()
+    const token = req.headers.authorization.split('Bearer ')[1]
+    if (!token) return res.status(401).end()
+    try {
+        const payload = await verifyToken(token)
+        console.log('payload', payload)
+        const user = await User.findById(payload.id)
+            .select('-password')
+            .lean()
+            .exec()
+        req.user = user
+        next()
+    } catch (e) {
+        console.error(e)
+        return res.status(401).end()
+    }
 }
