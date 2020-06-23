fb8cf36a1d797e7a5d945c2f451dbde3667bc982
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-23 09:21:08 +0300

    feat: balance in user model

--- src/recources/user/user.model.js
+++ src/recources/user/user.model.js
@@ -2,71 +2,76 @@ import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-            default: 'email',
-        },
-        role: {
-            type: String,
-            required: true,
-            enum: ['admin', 'user'],
-            default: 'user',
-        },
-        settings: {
-            theme: {
-                type: String,
-                required: false,
-                default: 'dark',
-            },
-            language: {
-                type: String,
-                required: false,
-                default: 'english',
-            },
-        },
-    },
-    { timestamps: true }
+	{
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			default: 'email'
+		},
+		role: {
+			type: String,
+			required: true,
+			enum: ['admin', 'user'],
+			default: 'user'
+		},
+		balance: {
+			type: Number,
+			required: true,
+			default: 0
+		},
+		settings: {
+			theme: {
+				type: String,
+				required: false,
+				default: 'dark'
+			},
+			language: {
+				type: String,
+				required: false,
+				default: 'english'
+			}
+		}
+	},
+	{ timestamps: true }
 )
 
-userSchema.pre('save', function (next) {
-    if (!this.isModified('password')) {
-        return next()
-    }
+userSchema.pre('save', function(next) {
+	if (!this.isModified('password')) {
+		return next()
+	}
 
-    bcrypt.hash(this.password, 8, (err, hash) => {
-        if (err) {
-            return next(err)
-        }
+	bcrypt.hash(this.password, 8, (err, hash) => {
+		if (err) {
+			return next(err)
+		}
 
-        this.password = hash
-        next()
-    })
+		this.password = hash
+		next()
+	})
 })
 
-userSchema.methods.checkPassword = function (password) {
-    const passwordHash = this.password
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(password, passwordHash, (err, same) => {
-            if (err) {
-                return reject(err)
-            }
+userSchema.methods.checkPassword = function(password) {
+	const passwordHash = this.password
+	return new Promise((resolve, reject) => {
+		bcrypt.compare(password, passwordHash, (err, same) => {
+			if (err) {
+				return reject(err)
+			}
 
-            resolve(same)
-        })
-    })
+			resolve(same)
+		})
+	})
 }
 
 export const User = mongoose.model('user', userSchema, 'users')
