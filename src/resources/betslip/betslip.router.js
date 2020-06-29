0f3e9c7bb02c9be4b85b7304684005120da368be
Joona Piirainen <joona.piirainen@gmail.com>
2020-06-29 11:33:05 +0300

    betslip + update user route in progress

--- src/resources/betslip/betslip.router.js
+++ src/resources/betslip/betslip.router.js
@@ -1,24 +1,64 @@
 import { Router } from 'express'
 import controllers from './betslip.controllers'
 import { Betslip } from './betslip.model'
+import { User } from '../user/user.model'
 
 const router = Router()
 
 //find all bets from one user
-router.route('/user/:id').get(async (req, res, next) => {
-	try {
-		const doc = await Betslip.find({ createdBy: req.params.id })
+router
+    .route('/user/:id')
+    .get(async (req, res, next) => {
+        try {
+            const doc = await Betslip.find({
+                createdBy: req.params.id,
+            })
 
-		if (!doc) return next()
+            if (!doc) return next()
 
-		res.status(200).json({ data: doc })
-	} catch (e) {
-		return next(e)
-	}
-})
+            res.status(200).json({
+                data: doc,
+            })
+        } catch (e) {
+            return next(e)
+        }
+    })
+    //post betslip + change user balance
+    .post(async (req, res, next) => {
+        try {
+            const user = await User.findOne({
+                _id: req.params.id,
+            })
+                .lean()
+                .exec()
+            const userBalance = user.balance
+            const amount = req.body.amount
+            if (userBalance < amount) return res.status(400).end()
+            const betSlip = await Betslip.create({
+                ...req.body,
+            })
+
+            const updatedUser = await User.updateOne(
+                {
+                    _id: user._id,
+                },
+                {
+                    $set: {
+                        userBalance: userBalance - amount,
+                    },
+                }
+            )
+        } catch (e) {
+            return next(e)
+        }
+    })
 
 router.route('/').get(controllers.getMany).post(controllers.createOne)
 
-router.route('/:id').get(controllers.getOne).put(controllers.updateOne).delete(controllers.removeOne)
+router
+    .route('/:id')
+    .get(controllers.getOne)
+    .put(controllers.updateOne)
+    .delete(controllers.removeOne)
 
 export default router
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,64 +1,69 @@
 export const getOne = model => async (req, res, next) => {
-	try {
-		const doc = await model.findOne({ _id: req.params.id }).lean().exec()
+    try {
+        const doc = await model.findOne({ _id: req.params.id }).lean().exec()
 
-		if (!doc) return next()
+        if (!doc) return next()
 
-		res.status(200).json({ data: doc })
-	} catch (e) {
-		return next(e)
-	}
+        res.status(200).json({ data: doc })
+    } catch (e) {
+        return next(e)
+    }
 }
 
 export const getMany = model => async (req, res, next) => {
-	try {
-		const docs = await model.find().lean().exec()
+    try {
+        const docs = await model.find().lean().exec()
 
-		if (!docs) return next()
+        if (!docs) return next()
 
-		res.status(200).json({ data: docs })
-	} catch (e) {
-		return next(e)
-	}
+        res.status(200).json({ data: docs })
+    } catch (e) {
+        return next(e)
+    }
 }
 
 export const createOne = model => async (req, res, next) => {
-	try {
-		const doc = await model.create({ ...req.body })
-		res.status(200).json({ data: doc })
-	} catch (e) {
-		return next(e)
-	}
+    try {
+        const doc = await model.create({ ...req.body })
+        res.status(200).json({ data: doc })
+    } catch (e) {
+        return next(e)
+    }
 }
 
 export const updateOne = model => async (req, res, next) => {
-	try {
-		console.log(req.body)
-		const updatedDoc = await model.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).lean().exec()
-		if (!updatedDoc) return next()
+    try {
+        const updatedDoc = await model
+            .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+            .lean()
+            .exec()
+        if (!updatedDoc) return next()
 
-		res.status(200).json({ data: updatedDoc })
-	} catch (e) {
-		return next(e)
-	}
+        res.status(200).json({ data: updatedDoc })
+    } catch (e) {
+        return next(e)
+    }
 }
 
 export const removeOne = model => async (req, res, next) => {
-	try {
-		const removed = await model.findOneAndRemove({ _id: req.params.id }).lean().exec()
+    try {
+        const removed = await model
+            .findOneAndRemove({ _id: req.params.id })
+            .lean()
+            .exec()
 
-		if (!removed) return next()
+        if (!removed) return next()
 
-		res.status(200).json({ data: removed })
-	} catch (e) {
-		return next(e)
-	}
+        res.status(200).json({ data: removed })
+    } catch (e) {
+        return next(e)
+    }
 }
 
 export const crudControllers = model => ({
-	createOne: createOne(model),
-	getOne: getOne(model),
-	getMany: getMany(model),
-	updateOne: updateOne(model),
-	removeOne: removeOne(model)
+    createOne: createOne(model),
+    getOne: getOne(model),
+    getMany: getMany(model),
+    updateOne: updateOne(model),
+    removeOne: removeOne(model),
 })
