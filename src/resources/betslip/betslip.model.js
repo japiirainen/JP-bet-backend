46dd13d75ef1ede345e5a4fcca47ca483980a138
Joona Piirainen <joona.piirainen@gmail.com>
2020-07-13 11:02:17 +0300

    feat: closed field on betslip

--- src/resources/betslip/betslip.model.js
+++ src/resources/betslip/betslip.model.js
@@ -1,27 +1,33 @@
 import mongoose from 'mongoose'
 
 const betSlipSchema = new mongoose.Schema(
-	{
-		choice: {
-			type: String,
-			enum: ['1', 'X', '2', 'not selected'],
-			default: 'not selected',
-			required: true
-		},
-		amount: { type: Number, required: true },
+    {
+        choice: {
+            type: String,
+            enum: ['1', 'X', '2', 'not selected'],
+            default: 'not selected',
+            required: true,
+        },
+        amount: { type: Number, required: true },
 
-		targetMatch: {
-			type: mongoose.SchemaTypes.ObjectId,
-			ref: 'match',
-			required: true
-		},
-		createdBy: {
-			type: mongoose.SchemaTypes.ObjectId,
-			ref: 'user',
-			required: true
-		}
-	},
-	{ timestamps: true }
+        closed: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+
+        targetMatch: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'match',
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+    },
+    { timestamps: true }
 )
 
 export const Betslip = mongoose.model('betslip', betSlipSchema, 'betslip')
