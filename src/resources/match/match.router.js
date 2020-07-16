6df7e210f0d354e43d8bcf667913336b1cbf0476
Joona Piirainen <joona.piirainen@gmail.com>
2020-07-16 20:40:30 +0300

    feat: testing + routes 🛤

--- src/resources/match/match.router.js
+++ src/resources/match/match.router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import controllers from './match.controllers'
+import { ObjectId } from 'mongoose'
 import { Match } from './match.model'
 import { Betslip } from '../betslip/betslip.model'
 import { User } from '../user/user.model'
@@ -19,6 +20,36 @@ pubRouter.route('/:closed').get(async (req, res, next) => {
         return next(e)
     }
 })
+
+export async function processBet(result, bet) {
+    async function updateBet() {
+        return Betslip.updateOne(
+            {
+                _id: bet._id,
+            },
+            {
+                $set: {
+                    closed: true,
+                    result,
+                },
+            }
+        )
+    }
+    function updateUser() {
+        if (bet.choice !== result) return
+        return User.updateOne(
+            { _id: bet.createdBy },
+            {
+                $inc: {
+                    balance: +bet.projectedWin,
+                },
+            }
+        )
+    }
+    const results = await Promise.all([updateBet(), updateUser()])
+    return results
+}
+
 //close match + bet and set results
 router.route('/result/:id').put(async (req, res, next) => {
     try {
@@ -45,34 +76,11 @@ router.route('/result/:id').put(async (req, res, next) => {
                 message: 'no bets for this match',
             })
 
-        //update bets to be closed + set result
-        await Betslip.updateMany(
-            {
-                targetMatch: req.params.id,
-            },
-            {
-                $set: {
-                    closed: true,
-                    result: req.body.products.onextwo.result,
-                },
-            },
-            { new: true }
-        )
-            .lean()
-            .exec()
-        //update user balances if won
-        // const updateUser = await User.updateOne(
-        //     {
-        //         _id: bet.createdBy,
-        //     },
-        //     {
-        //         $inc: {
-        //             balance: -bet.projectedWin,
-        //         },
-        //     }
-        // )
+        //close betslip and update user balance
 
-        // console.log(users)
+        await Promise.all(
+            bets.map((bet) => processBet(req.body.products.onextwo.result, bet))
+        )
 
         res.status(200).send({
             closedMatch,
---
+++ test/match.router.spec.js
@@ -0,0 +1,21 @@
+import { processBet } from '../src/resources/match/match.router'
+import { expect } from 'chai'
+import connect from '../database/db'
+require('dotenv').config()
+
+describe('Match router', () => {
+    before(async () => {
+        await connect()
+    })
+
+    it('should process bet', async () => {
+        const result = await processBet('team1', {
+            _id: '5f10671467f962c4cf94250a',
+            choice: 'tie',
+            createdBy: '5f08104eb6fa8f1e464f3772',
+            projectedWin: 120,
+        })
+        expect(result).to.exist
+        console.log(result)
+    })
+})
